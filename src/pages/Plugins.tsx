ca899e2c72b5de9c227a48b14835e4c0e12816ef
Tobias Hienzsch <tobias@modern-circuits.com>
2022-04-25 01:20:08 +0200

    Improve plugin preview layout

--- src/pages/Plugins.tsx
+++ src/pages/Plugins.tsx
@@ -13,58 +13,78 @@ import asic_tone_logo from "../img/asic/asic_tone_logo.png";
 
 import "./Plugins.css";
 
+interface PluginPreviewProps {
+  name: string;
+  description: string;
+  img_vlcd: string;
+}
+
+const PluginPreview = (props: PluginPreviewProps) => {
+  return (
+    <div className="card card-primary ">
+      <h3 className="center">{props.name}</h3>
+      <hr />
+      <img src={props.img_vlcd} alt={props.name} className="center" />
+      <p className="center">{props.description}</p>
+    </div>
+  );
+};
+
+interface BundlePreviewProps {}
+
+const BundlePreview = (props: BundlePreviewProps) => {
+  return (
+    <div className="card card-primary ">
+      <h3 className="center">ASIC</h3>
+      <hr />
+      <p className="asic-bundle-description">
+        Designed for outstanding productions, great sound and a fast workflow.
+        Five high-quality audio effects that work as VST3, AudioUnit and AAX in
+        almost any DAW.
+      </p>
+      <div className="center">
+        <img src={asic_shape_logo} alt="ASIC Shape" />
+        <img src={asic_filter_logo} alt="ASIC Filter" />
+        <img src={asic_space_logo} alt="ASIC Space" />
+        <img src={asic_dynamic_logo} alt="ASIC Dynamic" />
+        <img src={asic_tone_logo} alt="ASIC Tone" />
+      </div>
+      <p>$219 Single Payment</p>
+      <p>$6/Month subscription</p>
+    </div>
+  );
+};
+
 export const Plugins = () => {
   return (
     <div className="page-content page-plugins">
       <div className="section-plugin-preview">
-        <div className="card card-primary ">
-          <h3 className="center">ASIC Shape</h3>
-          <hr />
-          <img src={asic_shape_vlcd} alt="ASIC Shape" className="center" />
-          <p className="center">High quality saturation</p>
-        </div>
-        <div className="card card-primary ">
-          <h3 className="center">ASIC</h3>
-          <hr />
-          <p>
-            Designed for outstanding productions, great sound and a fast
-            workflow. Five high-quality audio effects that work as VST3,
-            AudioUnit and AAX in almost any DAW.
-          </p>
-          <div className="center">
-            <img src={asic_shape_logo} alt="ASIC Shape" />
-            <img src={asic_filter_logo} alt="ASIC Filter" />
-            <img src={asic_space_logo} alt="ASIC Space" />
-            <img src={asic_dynamic_logo} alt="ASIC Dynamic" />
-            <img src={asic_tone_logo} alt="ASIC Tone" />
-          </div>
-          <p>$219 Single Payment</p>
-          <p>$6/Month subscription</p>
-        </div>
-        <div className="card card-primary ">
-          <h3 className="center">ASIC Tone</h3>
-          <hr />
-          <img src={asic_tone_vlcd} alt="ASIC Tone" className="center" />
-          <p className="center">High quality saturation</p>
-        </div>
-        <div className="card card-primary ">
-          <h3 className="center">ASIC Filter</h3>
-          <hr />
-          <img src={asic_filter_vlcd} alt="ASIC Filter" className="center" />
-          <p className="center">High quality saturation</p>
-        </div>
-        <div className="card card-primary ">
-          <h3 className="center">ASIC Space</h3>
-          <hr />
-          <img src={asic_space_vlcd} alt="ASIC Space" className="center" />
-          <p className="center">High quality saturation</p>
-        </div>
-        <div className="card card-primary ">
-          <h3 className="center">ASIC Dynamic</h3>
-          <hr />
-          <img src={asic_dynamic_vlcd} alt="ASIC Dynamic" className="center" />
-          <p className="center">High quality saturation</p>
-        </div>
+        <PluginPreview
+          name="Shape"
+          description="High quality saturation"
+          img_vlcd={asic_shape_vlcd}
+        />
+        <BundlePreview />
+        <PluginPreview
+          name="Tone"
+          description="Technical tuning device"
+          img_vlcd={asic_tone_vlcd}
+        />
+        <PluginPreview
+          name="Filter"
+          description="Precise equalizer"
+          img_vlcd={asic_tone_vlcd}
+        />
+        <PluginPreview
+          name="Space"
+          description="Intuitive room processor"
+          img_vlcd={asic_space_vlcd}
+        />
+        <PluginPreview
+          name="Dynamic"
+          description="Precise peak controller"
+          img_vlcd={asic_dynamic_vlcd}
+        />
       </div>
     </div>
   );
